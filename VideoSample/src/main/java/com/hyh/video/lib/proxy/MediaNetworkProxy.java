9ee5993907374d2a5909587886f77ca1f6989c9b
EricHyh <252882326@qq.com>
2019-04-12 18:59:37 +0800

    重构下载模块

---
+++ VideoSample/src/main/java/com/hyh/video/lib/proxy/MediaNetworkProxy.java
@@ -0,0 +1,17 @@
+package com.hyh.video.lib.proxy;
+
+import android.content.Context;
+
+/**
+ * @author Administrator
+ * @description
+ * @data 2019/4/11
+ */
+//https://www.ibm.com/developerworks/cn/java/l-javaproxy/index.html
+public class MediaNetworkProxy {
+
+    public MediaNetworkProxy(Context context, String url) {
+        //
+
+    }
+}
