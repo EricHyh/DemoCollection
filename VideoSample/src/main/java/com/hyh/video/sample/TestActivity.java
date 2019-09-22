780cce496142b49552c8d5110134c7143daeb195
Eric_He <hyh123147@gmail.com>
2019-09-22 22:05:57 +0800

    ...

---
+++ VideoSample/src/main/java/com/hyh/video/sample/TestActivity.java
@@ -0,0 +1,19 @@
+package com.hyh.video.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Eric_He on 2019/9/22.
+ */
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+    }
+}
---
+++ VideoSample/src/main/java/com/hyh/video/widget/VideoItemInfoView.java
@@ -0,0 +1,8 @@
+package com.hyh.video.widget;
+
+/**
+ * Created by Eric_He on 2019/9/22.
+ */
+
+public class VideoItemInfoView {
+}
